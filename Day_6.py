ab9a52472d95289a4233815dc99a175eead2a270
Atique Hossain <39723702+atiquehossain@users.noreply.github.com>
2020-11-02 22:17:01 -0800

    Day 6

---
+++ Day_6.py
@@ -0,0 +1,16 @@
+#Opp,object,class,constructor,method,inheritance,Encapsulation,Palymorphism
+
+
+######Object###########
+
+class human:
+    def char(self):
+        print("Name is"+self.name)
+        print("Gender is"+self.gender)
+        print("Age is "+self.age)
+
+
+r= human()
+r.char(1)
+
+
--- day_5.py
+++ day_5.py
@@ -7,17 +7,5 @@
 #Stack####
 #Push,Pop,Peek,IsEmpty,IsFull,search,Minimum,maximum,
 
-print("Stack python")
 
-print("Input the stack number:")
-x=int(input())
-
-stc=[x]
-top=-1
-
-if(top==x-1):
-    print("Overflow")
-else:
-    top+1
-    stc= input()
 
